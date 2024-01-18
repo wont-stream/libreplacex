af4a6de6a799709efd412047f7b9575ad84ba296
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-01-17 22:21:28 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,122 @@
+const replaceURL = (
+  secure = true,
+  librex = "librex.me",
+  port = "",
+  searchURL
+) => {
+  let url = new URL(searchURL);
+
+  let q = "";
+  let type = 0;
+
+  switch (url.host) {
+    case "www.google.com":
+      q = url.searchParams.get("q");
+
+      switch (url.searchParams.get("tbm")) {
+        case "isch":
+          type = 1;
+          break;
+        case "vid":
+          type = 2;
+        default:
+      }
+
+      break;
+    case "duckduckgo.com":
+      q = url.searchParams.get("q");
+
+      switch (url.searchParams.get("ia")) {
+        case "images":
+          type = 1;
+          break;
+        case "videos":
+          type = 2;
+        default:
+      }
+
+      break;
+    case "www.bing.com":
+      q = url.searchParams.get("q");
+
+      switch (url.pathname.replace("/", "").replace("/search", "")) {
+        case "images":
+          type = 1;
+          break;
+        case "videos":
+          type = 2;
+        default:
+      }
+
+      break;
+    case "search.yahoo.com":
+      q = url.searchParams.get("p");
+      break;
+    case "images.search.yahoo.com":
+      q = url.searchParams.get("p");
+      type = 1;
+      break;
+    case "video.search.yahoo.com":
+      q = url.searchParams.get("p");
+      type = 2;
+      break;
+    case "yandex.com":
+      q = url.searchParams.get("text");
+
+      switch (url.pathname.replace("/", "").replace("/search", "")) {
+        case "images":
+          type = 1;
+          break;
+        case "video":
+          type = 2;
+        default:
+      }
+
+      break;
+    case "www.ecosia.org":
+      q = url.searchParams.get("q");
+
+      switch (url.pathname.replace("/", "")) {
+        case "images":
+          type = 1;
+          break;
+        case "videos":
+          type = 2;
+        default:
+      }
+
+      break;
+    default:
+      q =
+        url.searchParams.get("q") ||
+        url.searchParams.get("search") ||
+        url.searchParams.get("text") ||
+        url.searchParams.get("p");
+      break;
+  }
+
+  const newURL = new URL(`${secure ? "https" : "http"}://${librex}`);
+  newURL.port = port;
+  newURL.searchParams.set("q", q);
+  newURL.searchParams.set("t", type);
+
+  return newURL;
+};
+
+// CommonJS export (Node.js)
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+  module.exports = replaceURL;
+}
+
+// ES6 module export (Browser)
+if (typeof exports === "object" && typeof exports.default === "undefined") {
+  Object.defineProperty(exports, "default", { value: replaceURL });
+} else if (typeof define === "function" && define.amd) {
+  // AMD module definition
+  define("replaceURL", [], function () {
+    return replaceURL;
+  });
+} else {
+  // Global variable (Browser)
+  window.wont_stream.replaceURL = replaceURL;
+}
